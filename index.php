b5f73ff9fb42ac601fff7ec09079bc97bf8c08b2
Dan97O <dan97-oddo@live.it>
2023-05-08 16:25:41 +0200

    add edit markup

--- index.php
+++ index.php
@@ -8,10 +8,10 @@
   <link rel="stylesheet" href="./assets/css/style.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-  <title>Document</title>
+  <title>TODO LIST</title>
 </head>
 
-<body class="vh-100">
+<body>
 
   <div id="app">
     <div class="container">
@@ -26,7 +26,9 @@
             </ul>
           </div>
         </div>
+        <!-- /col-6 -->
       </div>
+      <!-- /row -->
 
 
       <div class="row d-flex justify-content-center">
@@ -43,10 +45,18 @@
                 </div>
               </div>
             </div>
+            <!-- /card-body -->
           </div>
+          <!-- card -->
         </div>
+        <!-- /col-6 -->
       </div>
+      <!-- /row -->
 
+    </div>
+    <!-- /container -->
+  </div>
+  <!-- /app -->
 
       <!-- cdn axios -->
       <script src='https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js'></script>
